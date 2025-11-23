18db27afc7a18aa10f85ba8bf195ca9044627ee2
Jayson Guevarra <jzon_03@yahoo.com>
2025-11-23 13:26:14 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MainteSync';
   notificationCount = 5; // Example notification count
+  
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+    
+  constructor(private breakpointObserver: BreakpointObserver) {}
+  
+  ngOnInit() {}
 }
